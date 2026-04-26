ff93cdab545877682c40ba3b2b154cd8ac1ccfd6
rajatchourasia7 <rajatchourasiyaanand@gmail.com>
2026-04-26 20:20:13 +0530

    Set up Jest with Chrome API mocks for testing

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testEnvironment: "node",
+  setupFiles: ["./tests/chrome-mock.js"],
+};
---
+++ tests/chrome-mock.js
@@ -0,0 +1,40 @@
+const store = {};
+
+const chrome = {
+  storage: {
+    local: {
+      get: jest.fn((keys, callback) => {
+        const result = {};
+        const keyList = Array.isArray(keys) ? keys : [keys];
+        keyList.forEach((key) => {
+          if (store[key] !== undefined) {
+            result[key] = store[key];
+          }
+        });
+        if (callback) callback(result);
+        return Promise.resolve(result);
+      }),
+      set: jest.fn((items, callback) => {
+        Object.assign(store, items);
+        if (callback) callback();
+        return Promise.resolve();
+      }),
+    },
+  },
+  omnibox: {
+    onInputEntered: {
+      addListener: jest.fn(),
+    },
+  },
+  tabs: {
+    update: jest.fn(),
+  },
+};
+
+function resetStore() {
+  Object.keys(store).forEach((key) => delete store[key]);
+}
+
+global.chrome = chrome;
+
+module.exports = { chrome, store, resetStore };
